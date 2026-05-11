70a96b8705011d3648f6212d22b6d0a18516f9c5
Saeed <yasrebi.saeed@gmail.com>
2026-05-11 15:46:28 +0100

    feat: initialize Cobra CLI structure

--- cmd/reliq/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-const version = "0.1.0"
-
-func main() {
-	fmt.Printf("reliq %s\n", version)
-}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/saeedya/reliq/cmd"
+
+func main() {
+	cmd.Execute()
+}
